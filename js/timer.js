7b0ba0b30cac3f72d24218e746cf1b9765f52a5d
Artylo <matglishev@gmail.com>
2021-02-06 03:53:34 +0000

    header attempts

---
+++ js/timer.js
@@ -0,0 +1,42 @@
+console.log("Start of TIMER");
+
+var dAte = new Date;
+var localTime;
+var localOffset;
+var utc;
+localTime = dAte.getTime();
+localOffset = dAte.getTimezoneOffset() * 60000;
+utc = localOffset + localTime;
+
+var start = new Date(utc);
+start.setHours(18, 0, 0); // 18 - No Daylight Savings; 17 - Daylight Savings Time
+
+function pad(num)
+{
+return ("0" + parseInt(num)).substr(-2);
+}
+
+function tick()
+{
+	var currentDate = new Date();
+	var currentLocalTime = currentDate.getTime();
+	var currentLocalOffset = currentDate.getTimezoneOffset() * 60000;
+	var currentUtc = currentLocalTime + currentLocalOffset;
+	var now = new Date(currentUtc);
+	
+	console.log("TIMEZONES SET");
+	
+	if (now > start)// too late, go to tomorrow
+	{ 
+	  start.setDate(start.getDate() + 1);
+	}
+	var remain = ((start - now) / 1000);
+	var hh = pad((remain / 60 / 60) % 60);
+	var mm = pad((remain / 60) % 60);
+	var ss = pad(remain % 60);
+
+	console.log("NEXT SHOW STARTS IN: "+ hh + ":" + mm + ":" + ss);
+	setTimeout(tick, 1000);
+}
+console.log("EXITED TIMER");
+document.addEventListener('DOMContentLoaded', tick);
